f0d210be3fa1b19790e55b6de36ca133ca133b70
ajil23 <azrilpraya@gmail.com>
2025-04-20 11:53:11 +0800

    fix: get data pegawai from API

--- Http/Controllers/PegawaiController.php
+++ Http/Controllers/PegawaiController.php
@@ -5,6 +5,7 @@ namespace Modules\Pengaturan\Http\Controllers;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Http;
 use Modules\Pengaturan\Entities\Golongan;
 use Modules\Pengaturan\Entities\Jabatan;
 use Modules\Pengaturan\Entities\Pegawai;
@@ -15,13 +16,32 @@ class PegawaiController extends Controller
      * Display a listing of the resource.
      * @return Renderable
      */
-    public function index()
+    public function index(Request $request)
     {
-        $golongan = Golongan::all();
-        $jabatan_fungsional = Jabatan::where('tipe_jabatan', 'Fungsional')->get();
-        $jabatan_struktural = Jabatan::where('tipe_jabatan', 'Struktural')->get();
-        $pegawai = Pegawai::paginate(10);
-        return view('pengaturan::pegawai.index', compact('pegawai', 'jabatan_fungsional', 'jabatan_struktural', 'golongan'));
+        try {
+            $token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiNzdlNTg2NzdjMjExOGZlZmQ5M2JjOTQzMWE0NzEyYzJhZDE4MDIwNTkyY2IwYzU5YzlhMTQyYWVmNDU0YTMxNTY1M2U3M2NlNGZlNWFmZDkiLCJpYXQiOjE3NDE1ODg0OTQuOTgzNDE5LCJuYmYiOjE3NDE1ODg0OTQuOTgzNDI5LCJleHAiOjE3NzMxMjQ0OTQuODI0NTc0LCJzdWIiOiI5MTYiLCJzY29wZXMiOlsicmVhZG9ubHkiXX0.r5mIeRDLDbEr-jyORFuWt4pKpwtQ8guJHWkVcCB0XALpe4CLkX-HA_aA_kK1un0sybGxFAwDv86iqwVIQhII7d9sVv_b9_xJzqbPWu7LOGJ1lNiFueDpTrCc851DBuE8DCKmMHMX80kNc_jgTTLfpBBe0BR1FRyGi39dhwdhiTmxUxwpvwaD_rHbt6OKFRHohRTsHkP-A2SCVBkHRiTEYcCiaJBjyduL2LFvSgy9Lk4NRhiB_2Qmv-Mlg6juGEvDyqZUx5779_FWk2eT13xVDRklX3j9ErbA1dUg0p2eUcxnvjae3kPxN7HCcilK2nut1b3qXmGVu_OXcBrw7buJWM5q72oKjwsxQujIcDjUB4ar_SpLLZ8mFHq5zqgp9dH1Gyy5RZ2yBeXLGBzt2Hru3hnESdRalFAi2hlySJO-0fiqdE8mIvL4nl9AP6J_VZseqhP-gUV0VbeEiP3Tq5bj-9LHjcWkdpWaRMUyLXGzSL6iiOh_kyDlNrUIAZJuiMDt3ecIvowET9_t2huUJoJJkUhqPfGnllWmQmlOG5gjnGA8HzW4qXq-Bo2pM1zP7uBTZe4Qgv-JYiaVnCfv9KRksjz_Fy9cWTnqSJHFoyRmJOtfO93MnU7nuf2GyWNexafAY4xD9DmFVYKX8wB8rO7eB2S_2WRueQ3M6oojVzB829E';
+
+            $page = $request->query('page', 1);
+
+            $response = Http::withHeaders([
+                'Authorization' => 'Bearer ' . $token,
+            ])->get("https://sit.poliwangi.ac.id/v2/api/v1/sitapi/pegawai?page%5Bnumber%5D=$page");
+
+            if ($response->successful()) {
+                $result = $response->json();
+
+                return view('pengaturan::pegawai.index', [
+                    'pegawai' => $result['data'],
+                    'links' => $result['links'],
+                    'currentPage' => $result['current_page'],
+                    'lastPage' => $result['last_page']
+                ]);
+            } else {
+                return view('pengaturan::pegawai.index')->with('error', 'Gagal mengambil data dari API');
+            }
+        } catch (\Exception $e) {
+            return view('pengaturan::pegawai.index')->with('error', $e->getMessage());
+        }
     }
 
     /**
